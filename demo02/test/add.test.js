803c8c8118d3e586ec491ddc343ee40564f5ef77
Frank <lee0324@126.com>
2017-06-15 16:54:47 +0800

    :sparkles: :sparkles:

--- demo02/test/add.test.js
+++ demo02/test/add.test.js
@@ -11,9 +11,4 @@ describe('加法函数的测试', function () {
     expect(add(1, 0)).to.be.equal(1);
   });
 
-  // 添加乘法测试
-  it('任何数乘以0应该等于0', function () {
-    expect(mul(3, 0)).to.be.equal(0);
-  });
-
 });
--- demo02/test/dir/multiply.test.js
+++ demo02/test/dir/multiply.test.js
@@ -1,8 +1,20 @@
+// Mocha默认只执行test子目录下面第一层的测试用例，不会执行更下层的用例
+// 为了改变这种行为，就必须加上--recursive参数
+// 这时test子目录下面所有的测试用例----不管在哪一层----都会执行
+// 执行 mocha --recursive
+
 var multiply = require('../../src/multiply');
 var expect = require('chai').expect;
 
-describe('乘法函数的测试', function() {
-  it('1 乘 1 应该等于 1', function() {
+describe('乘法函数的测试', function () {
+
+  it('1 乘 1 应该等于 1', function () {
     expect(multiply(1, 1)).to.be.equal(1);
   });
+
+  // 添加乘法测试
+  it('任何数乘以0应该等于0', function () {
+    expect(mul(3, 0)).to.be.equal(0);
+  });
+
 })

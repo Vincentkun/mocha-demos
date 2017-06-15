ebc5983c51c1b36641640a453cbdf03d02b90bea
Frank <lee0324@126.com>
2017-06-15 16:31:20 +0800

    :tada: multiply test

--- demo02/test/add.test.js
+++ demo02/test/add.test.js
@@ -1,12 +1,19 @@
 var add = require('../src/add.js');
+var mul = require('../src/multiply.js');
 var expect = require('chai').expect;
 
-describe('加法函数的测试', function() {
-  it('1 加 1 应该等于 2', function() {
+describe('加法函数的测试', function () {
+  it('1 加 1 应该等于 2', function () {
     expect(add(1, 1)).to.be.equal(2);
   });
 
-  it('任何数加0应该等于自身', function() {
+  it('任何数加0应该等于自身', function () {
     expect(add(1, 0)).to.be.equal(1);
   });
+
+  // 添加乘法测试
+  it('任何数乘以0应该等于0', function () {
+    expect(mul(3, 0)).to.be.equal(0);
+  });
+  
 });
